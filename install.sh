994116d584854b7f23229dd106bf750e7c737738
Dana Silver <dsilver1221@gmail.com>
2015-08-23 02:00:06 -0400

    Create app with resources and icon.

--- install.sh
+++ install.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-echo "export GIPHY_MESSAGES_HOME=`pwd`" >> ~/.bashrc
-
-cp "Giphy.applescript" ~/Library/Application\ Scripts/com.apple.iChat/
-
-exit 0
+cp "./../Resources/Giphy.applescript" ~/Library/Application\ Scripts/com.apple.iChat/
